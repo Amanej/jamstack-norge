8750a940a10e2bb607bac6772f0c0e42258dbe60
Aman <aman.mender@tegningtil3d.no>
2018-12-26 13:14:17 -0800

    Basic setup of frontpage in nuxt

---
+++ layouts/default.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div class="header">
+      <div class="logo">
+        <img src="~assets/img/jamstack-full-logo.svg" />
+        <span>Norge</span>
+      </div>
+      <nav>
+        <nuxt-link to="blogg">Blogg</nuxt-link>
+        <a 
+          href="https://www.meetup.com/JAMstack-Oslo/" 
+          target="_blank"
+        >Meetup</a>
+        <nuxt-link to="about">Om</nuxt-link>
+        <nuxt-link to="kontakt">Kontakt</nuxt-link>
+      </nav>
+    </div>
+    <nuxt/>
+  </div>
+</template>
+
+<style lang="scss">
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+div.header {
+  display: flex;
+  justify-content: flex-end;
+  justify-content: space-between;
+  div.logo {
+    margin: 12px;
+    span {
+      font-size: 28px;
+      font-weight: 100;
+      font-style: italic;      
+    }
+  }
+  nav {
+    //margin-right: 10px;
+    margin-right: 35px;
+    margin-top: 20px;
+    a {
+      margin: 10px;
+      text-decoration: none;
+      color: #333;
+    }
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="frontpage">
+    <section class="whatSection">
+      <h2>Hva er Jamstack ?</h2>
+      <p>Moderne webutvikling arkitektur basert på JavaScript, APIer og Markup</p>
+    </section>
+    <section class="whySection">
+      <h2>Hvorfor Jamstack ?</h2>
+      <ul>
+        <li>Bedre ytelse</li>
+        <li>Høyere sikkerhet</li>
+        <li>Enklet å skalere</li>
+        <li>Moderne utviklingsmiljø</li>
+      </ul>
+    </section>
+    <!-- Latest -->
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+section {
+  padding: 5% 5% 5% 5%;
+  text-align: center;  
+  color: white;
+  h2 {
+    font-weight: normal;
+    text-align: left;
+    margin-bottom: 20px;
+  }
+  p {
+    font-size: 1.4em;
+    padding: 20px 20%;
+    color: #333;
+    margin-bottom: 20px;
+  }
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    list-style-type: none;
+    padding-left: 0; 
+    padding: 20px 0;
+    li {
+      flex: 1;
+      font-size: 1.4em;
+      flex: 1 0 50%;
+      //margin-bottom: 20px;
+      margin-bottom: 40px;
+    }
+  }
+}
+section.whatSection {
+  background: #63a5be;
+}
+section.whySection {
+  background: #1da01d;
+}
+</style>
